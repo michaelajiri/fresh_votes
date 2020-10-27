3bcc6c8182db4e6fe0a0d27217fa54395aed8b7c
michaelajiri <michaelajiri@gmail.com>
2020-10-27 15:55:11 +0100

    getting started wih spring security

---
+++ fresh_votes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.freshvotes.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("mayk89@gmail.com")
+		.password("demo")
+		.roles("USER");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/").permitAll()
+		.anyRequest().hasRole("USER")
+		.and()
+		.formLogin()
+		.loginPage("/login")
+		.and()
+		.logout().logoutUrl("/logout")
+		.permitAll();
+	}
+}
