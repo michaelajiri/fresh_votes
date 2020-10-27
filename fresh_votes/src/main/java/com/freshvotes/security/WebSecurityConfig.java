1b6881cf8625f912730116d2a1f9e3d8f8802c50
michaelajiri <michaelajiri@gmail.com>
2020-10-27 16:43:34 +0100

    Creating custom login page

--- fresh_votes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
+++ fresh_votes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.anyRequest().hasRole("USER")
 		.and()
 		.formLogin()
-		.loginPage("/login")
+		.loginPage("/login").permitAll()
 		.and()
 		.logout().logoutUrl("/logout")
 		.permitAll();
---
+++ fresh_votes/src/main/java/com/freshvotes/web/DashboardController.java
@@ -0,0 +1,13 @@
+package com.freshvotes.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+	@GetMapping(value = "/")
+	public String homePage() {
+		return "index";
+	}
+}
