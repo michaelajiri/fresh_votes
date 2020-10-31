2d66627a6928af41485d4c8e88d94f7798eecc52
michaelajiri <michaelajiri@gmail.com>
2020-10-31 08:50:26 +0100

    Added Nav bar and FreshVotes image logo

--- fresh_votes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
+++ fresh_votes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
@@ -37,6 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.authorizeRequests()
 		.antMatchers("/").permitAll()
 		.antMatchers("/register").permitAll()
+		.antMatchers("/images/**").permitAll()
 		.antMatchers("/admin/**").hasRole("ADMIN")
 		.antMatchers("/products/**").hasAnyRole("ADMIN", "USER")
 		.and()
